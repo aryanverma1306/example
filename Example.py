927fea61cd49ac6075b12a36c7a6c1bda9cd47da
aryanverma1306 <43494205+aryanverma1306@users.noreply.github.com>
2022-11-22 23:07:23 +0100

    Add files via upload

---
+++ Example.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import gradio as gr
+
+def sepia(input_img):
+    sepia_filter = np.array([
+        [0.393, 0.769, 0.189], 
+        [0.349, 0.686, 0.168], 
+        [0.272, 0.534, 0.131]
+    ])
+    sepia_img = input_img.dot(sepia_filter.T)
+    sepia_img /= sepia_img.max()
+    return sepia_img
+
+demo = gr.Interface(sepia, gr.Image(shape=(600, 600)), "image")
+demo.launch()
+
+iface = gr.Interface(
+    image_classifier, 
+    gr.inputs.Image(shape=(224, 224)), 
+    gr.outputs.Label(num_top_classes=3),
+    capture_session=True,
+    )
+
+
+# In[ ]:
+
+
+
+
